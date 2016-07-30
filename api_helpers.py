0977ee8813e64028e400eed54a8331b71342519f
MilkshakePanda <iam.superpeluche@gmail.com>
2016-07-30 17:09:39 +0200

    Refactor display_results and make_request functions

--- api_helpers.py
+++ api_helpers.py
@@ -1,17 +1,21 @@
 import requests
 import webbrowser
 
+
+def display_results(request):
+    user_input = input("Do you want to see the results in the console or in the browser\n")
+
+    while user_input != "console" and user_input != 'browser':
+        print("This is a required field")
+        user_input = input("Do you want to see the results in the console or in the browser\n")
+
+    if user_input == 'console':
+        print(request.json())
+    else:
+        webbrowser.open(request.url)
+
 def make_request(endpoint, data):
     url = "https://api.foursquare.com/v2/venues/" + endpoint
     request = requests.get(url, params=data)
-    return request
-
-def show_results(request):
-    url = request.url
-    return webbrowser.open(url)
-
-def display_results(request):
-    # pretty print the json
-    response = request.json()
-    print(response)
+    display_results(request)
 
