85f6db18ab88dd13130cf3f298aba007bf8e373d
MilkshakePanda <iam.superpeluche@gmail.com>
2016-08-03 16:24:28 +0200

    Create package

--- fsq_wrapper/api_helpers.py
+++ fsq_wrapper/api_helpers.py
@@ -2,9 +2,13 @@ import requests
 import webbrowser
 
 
-def display_results(request):
-    user_input = input("Do you want to see the results in the console or in the browser\n")
+def make_request(endpoint, data):
+    url = "https://api.foursquare.com/v2/venues/" + endpoint
+    request = requests.get(url, params=data)
+    display_results(request)
 
+def display_results(request):
+    user_input = raw_input("Do you want to see the results in the console or in the browser\n")
     while user_input != "console" and user_input != 'browser':
         print("This is a required field")
         user_input = input("Do you want to see the results in the console or in the browser\n")
@@ -13,9 +17,3 @@ def display_results(request):
         print(request.json())
     else:
         webbrowser.open(request.url)
-
-def make_request(endpoint, data):
-    url = "https://api.foursquare.com/v2/venues/" + endpoint
-    request = requests.get(url, params=data)
-    display_results(request)
-
