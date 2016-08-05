a7abe065f0a03cd018f8a4c4b601a1057111b97a
MilkshakePanda <iam.superpeluche@gmail.com>
2016-08-05 22:20:58 +0200

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 
     name="fsq_wrapper",
-    version="0.0.4",
-    description="This is a CLI tool to make calls to the foursquare API",
+    version="0.0.5",
+    description="Test This is a CLI tool to make calls to the foursquare API",
     url="https://github.com/Zabanaa/foursquare-cli",
     author="Karim Cheurfi (Zabanaa)",
     author_email="karim.cheurfi@gmail.com",
